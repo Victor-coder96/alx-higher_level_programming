734096d37ce636daa5ffb3c6d7cca231c3715d45
victor-coder96 <vesionyeaba@gmail.com>
2023-05-21 05:58:11 -0700

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-
-if number >= 0:
-    l_digit = number % 10
-else:
-    l_digit = number % -10
-
-print("Last digit of {} is {}".format(number, l_digit), end='')
-
-if l_digit > 5:
-    print(" and is greater than 5")
-elif l_digit == 0:
-    print(" and is 0")
-else:
-    print(" and is less than 6 and not 0")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
